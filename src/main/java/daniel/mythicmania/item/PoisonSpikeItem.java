2a1fd7f689f059981e220d2e33efafc4a8680b36
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-07-29 15:40:38 -0400

    Add Poison Spike (take 2)

---
+++ src/main/java/daniel/mythicmania/item/PoisonSpikeItem.java
@@ -0,0 +1,10 @@
+package daniel.mythicmania.item;
+
+import daniel.mythicmania.MythicMania;
+import net.minecraft.item.Item;
+
+public class PoisonSpikeItem extends Item {
+    public PoisonSpikeItem() {
+        super(new Item.Settings().group(MythicMania.MYTHICMANIA_ITEM_GROUP));
+    }
+}
