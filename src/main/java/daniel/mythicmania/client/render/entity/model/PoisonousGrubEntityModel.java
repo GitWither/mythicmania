b1c336e86ac66a04d10ee51ff2ed71f97a62c47d
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-07-31 20:10:04 -0400

    Add animation to Poisonous Grub
    Edited the following files: PoisonousGrubEntityModel.java

--- src/main/java/daniel/mythicmania/client/render/entity/model/PoisonousGrubEntityModel.java
+++ src/main/java/daniel/mythicmania/client/render/entity/model/PoisonousGrubEntityModel.java
@@ -5,13 +5,25 @@ import net.minecraft.client.model.*;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.MathHelper;
 
 public class PoisonousGrubEntityModel extends EntityModel<PoisonousGrubEntity> {
 
     private final ModelPart root;
 
+    private final ModelPart segment1;
+    private final ModelPart segment2;
+    private final ModelPart segment3;
+    private final ModelPart segment4;
+    private final ModelPart segment5;
+
     public PoisonousGrubEntityModel(ModelPart part) {
         root = part;
+        segment1 = root.getChild("grub_poison").getChild("bodyseg2");
+        segment2 = root.getChild("grub_poison").getChild("bodyseg2").getChild("bodyseg3");
+        segment3 = root.getChild("grub_poison").getChild("bodyseg2").getChild("bodyseg3").getChild("tail");
+        segment4 = root.getChild("grub_poison").getChild("bodyseg1");
+        segment5 = root.getChild("grub_poison").getChild("bodyseg1").getChild("head");
     }
 
     public static TexturedModelData getTexturedModelData() {
@@ -42,7 +54,11 @@ public class PoisonousGrubEntityModel extends EntityModel<PoisonousGrubEntity> {
 
     @Override
     public void setAngles(PoisonousGrubEntity entity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch) {
-
+        segment1.yaw = MathHelper.sin(animationProgress * 0.8f) * 0.1f;
+        segment2.yaw = MathHelper.cos(animationProgress * 0.8f) * 0.04f;
+        segment3.yaw = MathHelper.sin(animationProgress * 0.9f) * 0.03f;
+        segment4.yaw = MathHelper.sin(animationProgress * 0.8f) * 0.04f;
+        segment5.yaw = MathHelper.cos(animationProgress * 0.8f) * 0.08f;
     }
 
     @Override
