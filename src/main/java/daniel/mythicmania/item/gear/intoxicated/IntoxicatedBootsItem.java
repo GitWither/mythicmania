c1b7952ee98cdcc076ec85016ccc8cfc24483f80
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-08-03 17:26:16 -0400

    Add Intoxicated Set (Functionless for now)
    Also adjusted Rinth in lang file (Rinth Plant -> Rinth)

---
+++ src/main/java/daniel/mythicmania/item/gear/intoxicated/IntoxicatedBootsItem.java
@@ -0,0 +1,10 @@
+package daniel.mythicmania.item.gear.intoxicated;
+
+import daniel.mythicmania.MythicMania;
+import net.minecraft.item.Item;
+
+public class IntoxicatedBootsItem extends Item {
+    public IntoxicatedBootsItem() {
+        super(new Item.Settings().group(MythicMania.MYTHICMANIA_ITEM_GROUP));
+    }
+}
---
+++ src/main/java/daniel/mythicmania/item/gear/intoxicated/IntoxicatedChestplateItem.java
@@ -0,0 +1,10 @@
+package daniel.mythicmania.item.gear.intoxicated;
+
+import daniel.mythicmania.MythicMania;
+import net.minecraft.item.Item;
+
+public class IntoxicatedChestplateItem extends Item {
+    public IntoxicatedChestplateItem() {
+        super(new Item.Settings().group(MythicMania.MYTHICMANIA_ITEM_GROUP));
+    }
+}
