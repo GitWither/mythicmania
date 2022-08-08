438fc491dfe2ce0adf5c84427be84e2d014ff72a
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-08-08 10:52:48 -0400

    Zapping Beetle changes
    + update lang file
    + add antenna of zapping beetle item
    + make zapping beetle drop 1-2 antennas of zapping beetle

---
+++ src/main/java/daniel/mythicmania/item/AntennaOfZappingBeetleItem.java
@@ -0,0 +1,10 @@
+package daniel.mythicmania.item;
+
+import daniel.mythicmania.MythicMania;
+import net.minecraft.item.Item;
+
+public class AntennaOfZappingBeetleItem extends Item {
+    public AntennaOfZappingBeetleItem() {
+        super(new Item.Settings().group(MythicMania.MYTHICMANIA_ITEM_GROUP));
+    }
+}
