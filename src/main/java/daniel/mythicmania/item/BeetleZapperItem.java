e8b5ea2e3eb9ce174ab3b8e996f4097ffce9c249
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-08-11 14:28:29 -0400

    Added beetle zapper, fix armor textures

---
+++ src/main/java/daniel/mythicmania/item/BeetleZapperItem.java
@@ -0,0 +1,10 @@
+package daniel.mythicmania.item;
+
+import daniel.mythicmania.MythicMania;
+import net.minecraft.item.Item;
+
+public class BeetleZapperItem extends Item {
+    public BeetleZapperItem() {
+        super(new Item.Settings().group(MythicMania.MYTHICMANIA_ITEM_GROUP));
+    }
+}
