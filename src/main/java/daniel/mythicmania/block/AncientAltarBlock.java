87d3e64f5442e8bd5ccc3b1f1db68bfd5c41e5e6
Daniel <wither.tweets@gmail.com>
2022-08-19 02:46:53 +0200

    Added Facing property to ancient altar

--- src/main/java/daniel/mythicmania/block/AncientAltarBlock.java
+++ src/main/java/daniel/mythicmania/block/AncientAltarBlock.java
@@ -4,13 +4,26 @@ import daniel.mythicmania.block.entity.AncientAltarBlockEntity;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.state.StateManager;
 import net.minecraft.util.math.BlockPos;
 import org.jetbrains.annotations.Nullable;
 
-public class AncientAltarBlock extends Block implements BlockEntityProvider {
+public class AncientAltarBlock extends HorizontalFacingBlock implements BlockEntityProvider {
     public AncientAltarBlock() {
         super(FabricBlockSettings.of(Material.STONE, MapColor.GRAY).nonOpaque().sounds(BlockSoundGroup.LODESTONE));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(HorizontalFacingBlock.FACING);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(FACING, ctx.getPlayerFacing());
     }
 
     @Nullable
