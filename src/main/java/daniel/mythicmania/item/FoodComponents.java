2653f43af7bbae9193ac6f4c1f916ffce4c9b390
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-09-10 20:10:07 -0400

    Fixed bug of Sweetened Berry inventory stack not decrementing by 1

--- src/main/java/daniel/mythicmania/item/FoodComponents.java
+++ src/main/java/daniel/mythicmania/item/FoodComponents.java
@@ -1,36 +1,37 @@
 package daniel.mythicmania.item;
 
-import net.minecraft.advancement.criterion.Criteria;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.FoodComponent;
-import net.minecraft.item.ItemStack;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.stat.Stats;
-import net.minecraft.world.World;
 
 public class FoodComponents {
 
     public static final FoodComponent POISONOUS_BERRY = new FoodComponent.Builder()
             .saturationModifier(1.1f)
+            .hunger(1)
             .statusEffect(new StatusEffectInstance(StatusEffects.POISON, 20*3, 1), 1)
             .build();
 
     public static final FoodComponent TRIBUS_FRUIT = new FoodComponent.Builder()
             .saturationModifier(1.1f)
+            .hunger(3)
             .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 10*20, 0), 1)
             .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 8*20, 0), 1)
             .build();
 
-    public static final FoodComponent ENCHANTED_TRIBUS_FRUIT = (new FoodComponent.Builder())
+    public static final FoodComponent ENCHANTED_TRIBUS_FRUIT = new FoodComponent.Builder()
             .hunger(4)
             .saturationModifier(1.2F)
             .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 50*20, 2), 1)
             .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 50*20, 2), 1)
             .statusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 60*20, 0), 1)
             .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 40*20, 3), 1)
+            .alwaysEdible()
+            .build();
+
+    public static final FoodComponent SWEETENED_BERRY = new FoodComponent.Builder()
+            .saturationModifier(1.1f)
+            .hunger(4)
             .alwaysEdible()
             .build();
 }
--- src/main/java/daniel/mythicmania/item/SweetenedBerryItem.java
+++ src/main/java/daniel/mythicmania/item/SweetenedBerryItem.java
@@ -6,18 +6,13 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUsage;
-import net.minecraft.item.Items;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.stat.Stats;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
-import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
 
 public class SweetenedBerryItem extends Item {
     public SweetenedBerryItem() {
-        super(new Item.Settings().group(MythicMania.MYTHICMANIA_ITEM_GROUP));
+        super(new Item.Settings().food(FoodComponents.SWEETENED_BERRY).group(MythicMania.MYTHICMANIA_ITEM_GROUP));
     }
 
     @Override
@@ -26,28 +21,30 @@ public class SweetenedBerryItem extends Item {
             Criteria.CONSUME_ITEM.trigger(serverPlayerEntity, stack);
             serverPlayerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
         }
+
         if (user instanceof PlayerEntity && !((PlayerEntity)user).getAbilities().creativeMode) {
             stack.decrement(1);
         }
+
         if (!world.isClient) {
             user.clearStatusEffects();
         }
 
-        return ItemStack.EMPTY;
+        return stack;
     }
 
-    @Override
-    public int getMaxUseTime(ItemStack stack) {
-        return 22;
-    }
-
-    @Override
-    public UseAction getUseAction(ItemStack stack) {
-        return UseAction.EAT;
-    }
-
-    @Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        return ItemUsage.consumeHeldItem(world, user, hand);
-    }
+//    @Override
+//    public int getMaxUseTime(ItemStack stack) {
+//        return 22;
+//    }
+//
+//    @Override
+//    public UseAction getUseAction(ItemStack stack) {
+//        return UseAction.EAT;
+//    }
+//
+//    @Override
+//    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+//        return ItemUsage.consumeHeldItem(world, user, hand);
+//    }
 }
