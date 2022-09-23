2e733d14e38d31fbe764dedb683c096de9a4ce32
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-09-23 16:59:55 -0400

    Add emissive texture to wasted demon (#38)

--- src/main/java/daniel/mythicmania/client/render/entity/WastedDemonEntityRenderer.java
+++ src/main/java/daniel/mythicmania/client/render/entity/WastedDemonEntityRenderer.java
@@ -1,21 +1,20 @@
 package daniel.mythicmania.client.render.entity;
 
 import daniel.mythicmania.MythicMania;
-import daniel.mythicmania.MythicManiaClient;
 import daniel.mythicmania.client.render.entity.feature.EntityCapeFeatureRenderer;
-import daniel.mythicmania.client.render.entity.model.PoisonousGrubEntityModel;
 import daniel.mythicmania.client.render.entity.model.WastedDemonEntityModel;
-import daniel.mythicmania.client.render.entity.model.ZappingBeetleEntityModel;
+import daniel.mythicmania.client.render.entity.renderers.DemonEyesRenderer;
 import daniel.mythicmania.entity.*;
 import net.minecraft.client.render.entity.BipedEntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.Identifier;
 
-public class WastedDemonEntityRenderer extends BipedEntityRenderer<WastedDemonEntity, WastedDemonEntityModel> {
+public class WastedDemonEntityRenderer extends BipedEntityRenderer<WastedDemonEntity, WastedDemonEntityModel<Entity>> {
     public WastedDemonEntityRenderer(EntityRendererFactory.Context context) {
-        super(context, new WastedDemonEntityModel(context.getPart(MythicManiaEntityTypes.WASTED_DEMON_LAYER)), 0.5f);
+        super(context, new WastedDemonEntityModel<Entity>(context.getPart(MythicManiaEntityTypes.WASTED_DEMON_LAYER)), 0.5f);
         this.addFeature(new EntityCapeFeatureRenderer(this, this.getCapeTexture()));
+        this.addFeature(new DemonEyesRenderer<>(this));
     }
 
     @Override
