52f16620d27b92efd17b4668fa3522e1e65c137e
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-10-25 10:13:50 -0400

    Sort items into creative tabs (#55)

--- src/main/java/daniel/mythicmania/MythicMania.java
+++ src/main/java/daniel/mythicmania/MythicMania.java
@@ -12,11 +12,11 @@ public class MythicMania implements ModInitializer {
 
     public static final String MOD_ID = "mythicmania";
 
-    public static final ItemGroup MYTHICMANIA_ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier(MOD_ID, MOD_ID), () -> new ItemStack(Items.BEDROCK));
-    public static final ItemGroup MYTHICMANIA_BLOCK_ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "blocks"), () -> new ItemStack(Items.BEDROCK));
-    public static final ItemGroup MYTHICMANIA_ITEMS_ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "items"), () -> new ItemStack(Items.BEDROCK));
-    public static final ItemGroup MYTHICMANIA_COMBAT_ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "combat"), () -> new ItemStack(Items.BEDROCK));
-    public static final ItemGroup MYTHICMANIA_TOOLS_ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "tools"), () -> new ItemStack(Items.BEDROCK));
+    public static final ItemGroup MYTHICMANIA_BLOCK_ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "blocks"), () -> new ItemStack(MythicManiaItems.PULSATING_BLACKSTONE));
+    public static final ItemGroup MYTHICMANIA_ITEMS_ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "items"), () -> new ItemStack(MythicManiaItems.EMPTY_ORB));
+    public static final ItemGroup MYTHICMANIA_COMBAT_ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "combat"), () -> new ItemStack(MythicManiaItems.INTOXICATED_SWORD));
+    public static final ItemGroup MYTHICMANIA_TOOLS_ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "tools"), () -> new ItemStack(MythicManiaItems.INTOXICATED_AXE));
+    public static final ItemGroup MYTHICMANIA_FOOD_ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "food"), () -> new ItemStack(MythicManiaItems.TRIBUS_FRUIT));
 
     @Override
     public void onInitialize() {
--- src/main/java/daniel/mythicmania/item/EnchantedTribusFruitItem.java
+++ src/main/java/daniel/mythicmania/item/EnchantedTribusFruitItem.java
@@ -7,7 +7,7 @@ import net.minecraft.item.ItemStack;
 public class EnchantedTribusFruitItem extends Item {
 
     public EnchantedTribusFruitItem(boolean hasGlint) {
-        super(new Item.Settings().food(FoodComponents.ENCHANTED_TRIBUS_FRUIT).group(MythicMania.MYTHICMANIA_ITEM_GROUP));
+        super(new Item.Settings().food(FoodComponents.ENCHANTED_TRIBUS_FRUIT).group(MythicMania.MYTHICMANIA_FOOD_ITEM_GROUP));
     }
 
     public boolean hasGlint(ItemStack stack) {
