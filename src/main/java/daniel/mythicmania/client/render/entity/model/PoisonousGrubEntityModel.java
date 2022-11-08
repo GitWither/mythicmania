4c105aadcf5e2c6b5fcd5d2e096368ab7a51c6eb
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-11-07 19:52:50 -0500

    Touch up poison grub animation, fix item group for demon essence

--- src/main/java/daniel/mythicmania/client/render/entity/model/PoisonousGrubEntityModel.java
+++ src/main/java/daniel/mythicmania/client/render/entity/model/PoisonousGrubEntityModel.java
@@ -34,32 +34,32 @@ public class PoisonousGrubEntityModel extends EntityModel<PoisonousGrubEntity> {
         ModelPartData modelPartData = modelData.getRoot();
         ModelPartData grub_poison = modelPartData.addChild("grub_poison", ModelPartBuilder.create(), ModelTransform.pivot(-0.0042F, 24.0F, -0.6275F));
 
-        ModelPartData bodyseg2 = grub_poison.addChild("bodyseg2", ModelPartBuilder.create().uv(0, 16).cuboid(-1.36F, -2.0F, 0.0F, 2.0F, 2.0F, 3.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        ModelPartData bodyseg2 = grub_poison.addChild("bodyseg2", ModelPartBuilder.create().uv(0, 16).cuboid(-1.36F, -2.0F, -1.5F, 2.0F, 2.0F, 3.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 1.5F));
 
-        ModelPartData bodyseg3 = bodyseg2.addChild("bodyseg3", ModelPartBuilder.create().uv(0, 9).cuboid(-2.2724F, 0.1031F, -0.059F, 4.0F, 3.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(-0.0176F, -3.1031F, 2.909F));
+        ModelPartData bodyseg3 = bodyseg2.addChild("bodyseg3", ModelPartBuilder.create().uv(0, 9).cuboid(-2.2724F, 0.1031F, -2.309F, 4.0F, 3.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(-0.0176F, -3.1031F, 3.659F));
 
         ModelPartData cube_r1 = bodyseg3.addChild("cube_r1", ModelPartBuilder.create().uv(2, 3).cuboid(0.0F, -0.9875F, -0.0125F, 0.0F, 1.0F, 1.0F, new Dilation(0.0F))
-                .uv(0, 2).cuboid(0.0F, -1.9875F, -1.0125F, 0.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(-0.9424F, 0.2406F, 2.6535F, 0.0F, 0.0F, -0.1004F));
+                .uv(0, 2).cuboid(0.0F, -1.9875F, -1.0125F, 0.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(-0.9424F, 0.2406F, 0.4035F, 0.0F, 0.0F, -0.1004F));
 
         ModelPartData cube_r2 = bodyseg3.addChild("cube_r2", ModelPartBuilder.create().uv(2, 2).cuboid(0.0F, -1.0375F, -0.0125F, 0.0F, 1.0F, 1.0F, new Dilation(0.0F))
-                .uv(0, 0).cuboid(0.0F, -2.0375F, -1.0125F, 0.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(0.8076F, 0.2906F, 1.4035F, 0.0F, 0.0F, 0.2705F));
+                .uv(0, 0).cuboid(0.0F, -2.0375F, -1.0125F, 0.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(0.8076F, 0.2906F, -0.8465F, 0.0F, 0.0F, 0.2705F));
 
-        ModelPartData tail = bodyseg3.addChild("tail", ModelPartBuilder.create().uv(14, 0).cuboid(-1.31F, -0.86F, 0.11F, 2.0F, 2.0F, 3.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0176F, 1.9631F, 3.821F));
+        ModelPartData tail = bodyseg3.addChild("tail", ModelPartBuilder.create().uv(14, 0).cuboid(-1.31F, -0.86F, -1.64F, 2.0F, 2.0F, 3.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0176F, 1.9631F, 3.321F));
 
-        ModelPartData bodyseg1 = grub_poison.addChild("bodyseg1", ModelPartBuilder.create().uv(0, 0).cuboid(-2.33F, 0.5375F, -1.4375F, 4.0F, 3.0F, 6.0F, new Dilation(0.0F)), ModelTransform.pivot(0.04F, -3.5375F, -4.5125F));
+        ModelPartData bodyseg1 = grub_poison.addChild("bodyseg1", ModelPartBuilder.create().uv(0, 0).cuboid(-2.33F, 0.5375F, -2.9125F, 4.0F, 3.0F, 6.0F, new Dilation(0.0F)), ModelTransform.pivot(0.04F, -3.5375F, -3.0375F));
 
         ModelPartData cube_r3 = bodyseg1.addChild("cube_r3", ModelPartBuilder.create().uv(0, 4).cuboid(0.0F, -0.3125F, -0.3125F, 0.0F, 1.0F, 1.0F, new Dilation(0.0F))
-                .uv(2, 0).cuboid(0.0F, -1.3125F, -1.3125F, 0.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 0.625F, 0.0F, 0.0F, 0.1222F));
+                .uv(2, 0).cuboid(0.0F, -1.3125F, -1.3125F, 0.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, -0.85F, 0.0F, 0.0F, 0.1222F));
 
-        ModelPartData head = bodyseg1.addChild("head", ModelPartBuilder.create().uv(12, 12).cuboid(-1.86F, -0.86F, -3.46F, 3.0F, 2.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(-0.04F, 2.3975F, -1.1125F));
+        ModelPartData head = bodyseg1.addChild("head", ModelPartBuilder.create().uv(12, 12).cuboid(-1.86F, -0.86F, -1.46F, 3.0F, 2.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(-0.04F, 2.3975F, -4.5875F));
         return TexturedModelData.of(modelData, 32, 32);
     }
 
     @Override
     public void setAngles(PoisonousGrubEntity entity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch) {
-        segment1.yaw = MathHelper.sin(animationProgress * 0.8f) * 0.1f;
-        segment2.yaw = MathHelper.cos(animationProgress * 0.8f) * 0.04f;
-        segment3.yaw = MathHelper.sin(animationProgress * 0.9f) * 0.03f;
+        segment1.yaw = MathHelper.cos(animationProgress * 0.8f) * 0.1f;
+        segment2.yaw = MathHelper.sin(animationProgress * 0.8f) * 0.04f;
+        segment3.yaw = MathHelper.cos(animationProgress * 0.9f) * 0.03f;
         segment4.yaw = MathHelper.sin(animationProgress * 0.8f) * 0.04f;
         segment5.yaw = MathHelper.cos(animationProgress * 0.8f) * 0.08f;
     }
