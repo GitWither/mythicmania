d2bd560cdee7d250f369d393f423750479d2f2b7
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-12-06 19:27:36 -0500

    Update Demon Guardian
    - Added random chance of getting a sword or a shield
    - Decreased speed a bit

--- src/main/java/daniel/mythicmania/entity/DemonGuardianEntity.java
+++ src/main/java/daniel/mythicmania/entity/DemonGuardianEntity.java
@@ -3,13 +3,21 @@ package daniel.mythicmania.entity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.*;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.passive.IronGolemEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.LocalDifficulty;
+import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 public class DemonGuardianEntity extends HostileEntity {
     public DemonGuardianEntity(EntityType<? extends DemonGuardianEntity> entityType, World world) {
@@ -28,6 +36,29 @@ public class DemonGuardianEntity extends HostileEntity {
     }
 
     @Override
+    protected void initEquipment(Random random, LocalDifficulty localDifficulty) {
+        // Generates a random number which determines whether the Demon Guardian has a sword, a shield, or nothing.
+        int randomLootInt = world.random.nextInt(3);
+
+        if (randomLootInt == 1) {
+            this.equipStack(EquipmentSlot.OFFHAND, new ItemStack(Items.SHIELD));
+        } else if (randomLootInt == 2) {
+            this.equipStack(EquipmentSlot.MAINHAND, new ItemStack(Items.STONE_SWORD));
+        }
+
+        super.initEquipment(random, localDifficulty);
+    }
+
+    @Nullable
+    @Override
+    public EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, @Nullable EntityData entityData, @Nullable NbtCompound entityNbt) {
+        Random random = world.getRandom();
+        this.initEquipment(random, difficulty);
+
+        return super.initialize(world, difficulty, spawnReason, entityData, entityNbt);
+    }
+
+    @Override
     public boolean tryAttack(Entity entity) {
         if (super.tryAttack(entity) && entity instanceof LivingEntity livingEntity) {
             return tryAttack(entity);
@@ -41,7 +72,7 @@ public class DemonGuardianEntity extends HostileEntity {
                 .createHostileAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 15)
                 .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 32.0D)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.40D)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.23D)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 3.0D)
                 .add(EntityAttributes.GENERIC_ARMOR_TOUGHNESS, 2);
     }
