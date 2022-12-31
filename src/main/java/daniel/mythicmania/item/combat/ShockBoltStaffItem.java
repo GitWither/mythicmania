f831481cda77d5624f156aba9af57c0f94707a6c
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-12-30 19:52:46 -0500

    Add some useful comments

--- src/main/java/daniel/mythicmania/item/combat/ShockBoltStaffItem.java
+++ src/main/java/daniel/mythicmania/item/combat/ShockBoltStaffItem.java
@@ -21,28 +21,38 @@ public class ShockBoltStaffItem extends SwordItem {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         if (!world.isClient) {
+
+            // Cooldown manager
             user.getItemCooldownManager().set(this, 8);
 
             ItemStack shockBoltItem = new ItemStack(MythicManiaItems.SHOCK_BOLT);
+
+            // Stores the projectiles in variables
             ShockBoltEntity projectile = new ShockBoltEntity(world, user);
             ShockBoltEntity projectile2 = new ShockBoltEntity(world, user);
             ShockBoltEntity projectile3 = new ShockBoltEntity(world, user);
 
             projectile.setItem(shockBoltItem);
 
+            // Sets the velocity of projectiles
             projectile.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 1, world.random.nextInt(4));
             projectile2.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 1, world.random.nextInt(4));
             projectile3.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 1, world.random.nextInt(4));
 
+            // Checks if the user has the shock bolt item and is not in creative mode
             if (user.getInventory().contains(shockBoltItem) && !user.getAbilities().creativeMode) {
                 ItemStack wand = user.getStackInHand(Hand.MAIN_HAND);
+
+                // Damage wand by 1
                 wand.damage(1, user, (p) -> p.sendToolBreakStatus(user.getActiveHand()));
 
                 user.getInventory().removeOne(shockBoltItem);
                 user.playerScreenHandler.sendContentUpdates();
             }
+
             world.playSound(null, user.getX(), user.getY(), user.getZ(), MythicManiaSoundEvents.SHOCK_BOLT_STAFF_FIRE, SoundCategory.NEUTRAL, 0.7F, 2.5F);
 
+            // Summon projectiles
             world.spawnEntity(projectile);
             world.spawnEntity(projectile2);
             world.spawnEntity(projectile3);
