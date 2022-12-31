e2f594e1a3c1a2458327a085a6755ea33b1143f9
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-12-30 19:34:03 -0500

    Implement durability loss and requirement to have projectile in inventory

--- src/main/java/daniel/mythicmania/item/combat/ShockBoltStaffItem.java
+++ src/main/java/daniel/mythicmania/item/combat/ShockBoltStaffItem.java
@@ -3,6 +3,7 @@ package daniel.mythicmania.item.combat;
 import daniel.mythicmania.client.sound.MythicManiaSoundEvents;
 import daniel.mythicmania.entity.ShockBoltEntity;
 import daniel.mythicmania.item.MythicManiaItems;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
@@ -20,23 +21,34 @@ public class ShockBoltStaffItem extends SwordItem {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         if (!world.isClient) {
-            world.playSound(null, user.getX(), user.getY(), user.getZ(), MythicManiaSoundEvents.SHOCK_BOLT_STAFF_FIRE, SoundCategory.NEUTRAL, 0.7F, 2.5F);
             user.getItemCooldownManager().set(this, 8);
 
-            ItemStack itemStack = new ItemStack(MythicManiaItems.SHOCK_BOLT);
+            PlayerEntity playerEntity;
+
+            ItemStack shockBoltItem = new ItemStack(MythicManiaItems.SHOCK_BOLT);
             ShockBoltEntity projectile = new ShockBoltEntity(world, user);
             ShockBoltEntity projectile2 = new ShockBoltEntity(world, user);
             ShockBoltEntity projectile3 = new ShockBoltEntity(world, user);
 
-            projectile.setItem(itemStack);
+            projectile.setItem(shockBoltItem);
 
             projectile.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 1, world.random.nextInt(4));
             projectile2.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 1, world.random.nextInt(4));
             projectile3.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 1, world.random.nextInt(4));
 
-            world.spawnEntity(projectile);
-            world.spawnEntity(projectile2);
-            world.spawnEntity(projectile3);
+            if (user.getInventory().contains(shockBoltItem) && !user.getAbilities().creativeMode) {
+                world.spawnEntity(projectile);
+                world.spawnEntity(projectile2);
+                world.spawnEntity(projectile3);
+
+                world.playSound(null, user.getX(), user.getY(), user.getZ(), MythicManiaSoundEvents.SHOCK_BOLT_STAFF_FIRE, SoundCategory.NEUTRAL, 0.7F, 2.5F);
+
+                ItemStack wand = user.getStackInHand(Hand.MAIN_HAND);
+                wand.damage(1, user, (p) -> p.sendToolBreakStatus(user.getActiveHand()));
+
+                user.getInventory().removeOne(shockBoltItem);
+                user.playerScreenHandler.sendContentUpdates();
+            }
         }
 
         return super.use(world, user, hand);
