46a4c146f9398879f6aab5b9a7527f2d4005b669
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-02-20 11:31:50 -0500

    Spawn particles on hit & change itemgroup icon

--- src/main/java/daniel/mythicmania/item/combat/RuinousSwordItem.java
+++ src/main/java/daniel/mythicmania/item/combat/RuinousSwordItem.java
@@ -1,11 +1,14 @@
 package daniel.mythicmania.item.combat;
 
+import daniel.mythicmania.particle.MythicManiaParticles;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
 
 public class RuinousSwordItem extends SwordItem {
     StatusEffectInstance regeneration = new StatusEffectInstance(StatusEffects.REGENERATION, 20*3, 0);
@@ -15,8 +18,13 @@ public class RuinousSwordItem extends SwordItem {
 
     @Override
     public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        ServerWorld world = (ServerWorld) target.getWorld();
+
+        world.spawnParticles(ParticleTypes.FLAME, target.getX(), target.getY() + target.getEyeHeight(target.getPose()), target.getZ(), 5, 0.5, 0.2, 0.5, 0.02);
+
         attacker.addStatusEffect(regeneration);
         target.setOnFireFor(4);
+
         return super.postHit(stack, target, attacker);
     }
 }
