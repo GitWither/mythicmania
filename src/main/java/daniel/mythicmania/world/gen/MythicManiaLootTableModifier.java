847b4fe15243bf94864200e2b409f685946fa7be
Daniel <wither.tweets@gmail.com>
2023-06-06 22:44:48 +0200

    Minor rework for loot tables

--- src/main/java/daniel/mythicmania/world/gen/MythicManiaLootTableModifier.java
+++ src/main/java/daniel/mythicmania/world/gen/MythicManiaLootTableModifier.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.loot.v2.FabricLootTableBuilder;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.item.Item;
 import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTables;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
@@ -15,32 +16,26 @@ import net.minecraft.util.Identifier;
 public class MythicManiaLootTableModifier {
 	public static void registerLootTableModifiers() {
 		LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
-			initLootTableModifier("chests/village/village_desert_house", MythicManiaItems.WASTED_ESSENCE, 0.4f, id, tableBuilder);
-			initLootTableModifier("chests/abandoned_mineshaft", MythicManiaItems.WASTED_ESSENCE, 0.4f, id, tableBuilder);
-			initLootTableModifier("chests/village/village_weaponsmith", MythicManiaItems.WASTED_ESSENCE, 0.6f, id, tableBuilder);
-			initLootTableModifier("chests/village/village_toolsmith", MythicManiaItems.WASTED_ESSENCE, 0.6f, id, tableBuilder);
+			appendItemToLootTable(LootTables.VILLAGE_DESERT_HOUSE_CHEST, MythicManiaItems.WASTED_ESSENCE, 0.4f, id, tableBuilder);
+			appendItemToLootTable(LootTables.ABANDONED_MINESHAFT_CHEST, MythicManiaItems.WASTED_ESSENCE, 0.4f, id, tableBuilder);
+			appendItemToLootTable(LootTables.VILLAGE_WEAPONSMITH_CHEST, MythicManiaItems.WASTED_ESSENCE, 0.6f, id, tableBuilder);
+			appendItemToLootTable(LootTables.VILLAGE_TOOLSMITH_CHEST, MythicManiaItems.WASTED_ESSENCE, 0.6f, id, tableBuilder);
 
-			initLootTableModifier("chests/ancient_city", MythicManiaItems.CHARGED_RUINED_ORB, 0.5f, id, tableBuilder);
-			initLootTableModifier("chests/ancient_city", MythicManiaItems.CHARGED_NUCLEAR_ORB, 0.5f, id, tableBuilder);
+			appendItemToLootTable(LootTables.ANCIENT_CITY_CHEST, MythicManiaItems.CHARGED_RUINED_ORB, 0.5f, id, tableBuilder);
+			appendItemToLootTable(LootTables.ANCIENT_CITY_CHEST, MythicManiaItems.CHARGED_NUCLEAR_ORB, 0.5f, id, tableBuilder);
 		});
 	}
 
-	// TODO: Refactor this system. Perhaps get rid of redundant parameters.
-	public static void initLootTableModifier(String identifier,
-									  Item item,
-									  float chance,
-									  Identifier id,
-									  FabricLootTableBuilder tableBuilder) {
-		final Identifier path = new Identifier(identifier);
+	// TODO: Refactor this system.
+	public static void appendItemToLootTable(Identifier target, Item item, float chance, Identifier id, FabricLootTableBuilder tableBuilder) {
+		if (!target.equals(id)) return;
 
-		if (path.equals(id)) {
-			LootPool.Builder poolBuilder = LootPool.builder()
-				.rolls(ConstantLootNumberProvider.create(1))
-				.conditionally(RandomChanceLootCondition.builder(chance))
-				.with(ItemEntry.builder(item))
-				.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+		LootPool.Builder poolBuilder = LootPool.builder()
+			.rolls(ConstantLootNumberProvider.create(1))
+			.conditionally(RandomChanceLootCondition.builder(chance))
+			.with(ItemEntry.builder(item))
+			.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
 
-			tableBuilder.pool(poolBuilder.build());
-		}
+		tableBuilder.pool(poolBuilder.build());
 	}
 }
