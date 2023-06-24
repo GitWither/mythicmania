104a9528855f3b8ebc6baf54f0ea10a6ae63361c
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-06-24 08:51:17 -0400

    Update OrbiterEntity.java

--- src/main/java/daniel/mythicmania/entity/mob/OrbiterEntity.java
+++ src/main/java/daniel/mythicmania/entity/mob/OrbiterEntity.java
@@ -7,9 +7,6 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.ActiveTargetGoal;
-import net.minecraft.entity.attribute.DefaultAttributeContainer;
-import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.world.World;
 
 public class OrbiterEntity extends AbstractOrbiterEntity {
