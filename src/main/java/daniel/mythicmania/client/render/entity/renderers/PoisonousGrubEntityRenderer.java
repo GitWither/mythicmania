d5884ba96fd8348c61cb26bbd382c5ac5c26908f
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-07-05 19:35:05 -0400

    Add emissive textures for eyes of grubs

--- src/main/java/daniel/mythicmania/client/render/entity/renderers/PoisonousGrubEntityRenderer.java
+++ src/main/java/daniel/mythicmania/client/render/entity/renderers/PoisonousGrubEntityRenderer.java
@@ -1,6 +1,7 @@
 package daniel.mythicmania.client.render.entity.renderers;
 
 import daniel.mythicmania.MythicMania;
+import daniel.mythicmania.client.render.entity.feature.MythicManiaEmissiveFeatureRenderer;
 import daniel.mythicmania.client.render.entity.model.PoisonousGrubEntityModel;
 import daniel.mythicmania.entity.MythicManiaEntityTypes;
 import daniel.mythicmania.entity.mob.PoisonousGrubEntity;
@@ -9,12 +10,16 @@ import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.util.Identifier;
 
 public class PoisonousGrubEntityRenderer extends MobEntityRenderer<PoisonousGrubEntity, PoisonousGrubEntityModel> {
+    private static final Identifier TEXTURE = new Identifier(MythicMania.MOD_ID, "textures/entity/grubs/poisonous.png");
+    private static final Identifier EMISSIVE = new Identifier(MythicMania.MOD_ID, "textures/entity/grubs/poisonous_e.png");
+
     public PoisonousGrubEntityRenderer(EntityRendererFactory.Context context) {
         super(context, new PoisonousGrubEntityModel(context.getPart(MythicManiaEntityTypes.POISONOUS_GRUB_LAYER)), 0.5f);
+        this.addFeature(new MythicManiaEmissiveFeatureRenderer<>(this, EMISSIVE));
     }
 
     @Override
     public Identifier getTexture(PoisonousGrubEntity entity) {
-        return new Identifier(MythicMania.MOD_ID, "textures/entity/grubs/poisonous.png");
+        return TEXTURE;
     }
 }
